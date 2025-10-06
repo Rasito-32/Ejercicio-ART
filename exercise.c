7443ebf852cf4d5451ff1ebeb0e90013d4f341f0
RaSSito <rass08014@gmail.com>
2025-10-06 17:00:42 +0200

    Ejercicio ART completo

--- exercise.c
+++ exercise.c
@@ -1,42 +1,49 @@
-int random_five();          //Función que nos devuelve de manera aleatoría un número del 1 al 5 sin repetirse
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int random_seven()      //función que devuelve un aleatorio del 1 al 7
-{
-    int flag = 0;
-    int result = 0;
-    while (flag)
-    {
-        result = (random_five() - 1) * 5;   //25 posibilidades
-        if(result <= 21)                    //Si está dentro del 21, éxito
-            flag = 1;
-    }
-    
-    return result;
-}
+int uniform_rand(int n);
+int random_serven();
 
 int main()
 {
-    int numeros_sacados[7] = {0, 0, 0, 0, 0, 0, 0};
+    srand(time(NULL));
+    int numeros_sacados[10000];
     int terminado = 0;
-
-    while (terminado != 7)
+    for(int i = 0; i < 10000; i ++)
     {
-        int repetido = 0;
-        while (repetido)
-        {
-            int res = random_seven();
-            int i = 0;
-            for (i = 0; i < terminado; i++)
-            {
-                if(res == numeros_sacados[i])
-                    i = terminado;
-            }
-            if(res != numeros_sacados[terminado] && i == terminado)
-            {
-                repetido = 1;
-                numeros_sacados[terminado] = res;
-            }
-        }
-        terminado ++;
+        numeros_sacados[i] = random_seven();
     }
+
+    for(int i = 0; i < 7; i ++)
+    {
+        int output = 0;
+        for(int j = 0; j < 10000; j++)
+        {
+            if(numeros_sacados[j] == i + 1)
+                output ++;
+        }
+        float res = (float) (output/100);
+        printf("Apariciones del %i: %f\n", i + 1, res);
+    }
+}
+
+int uniform_rand(int n) {
+    int r;
+    int limit = RAND_MAX - (RAND_MAX % n);
+    do {
+        r = rand();
+    } while (r >= limit);
+    return r % n;
+}
+
+int random_seven()      //función que devuelve un aleatorio del 1 al 7
+{
+      int num;
+    do {
+        int a = uniform_rand(5);
+        int b = uniform_rand(5); 
+        num = a * 5 + b;     
+    } while (num >= 21);         
+    return num % 7 + 1;               
 }
--- hello.cu
+++ hello.cu
@@ -4,15 +4,15 @@
 #include <curand_kernel.h>
 #include <time.h>
 
-#define N 1024000
-#define num_threads 1024
+#define N 1000
+#define num_threads 1000
 
-int random_five(int thread, int seed);
+int random_five(int thread, curandState state);
 
-int random_seven(int thread);
+int random_seven(int thread, unsigned long long seed);
 
 __global__ void sum_kernel(int *Ad);
-__global__ void random_kernel(int *Ad);
+__global__ void random_kernel(int *Ad, unsigned long long seed);
 
 int main(void)
 {
@@ -25,7 +25,7 @@ int main(void)
     A = (int*) malloc(bytes);
     cudaMalloc(&Ad, bytes);
 
-    random_kernel<<<1000,num_threads>>>(Ad);
+    random_kernel<<<1,num_threads>>>(Ad, time(NULL));
     cudaMemcpy(A, Ad, bytes, cudaMemcpyDeviceToHost);
     
     cudaDeviceSynchronize();
@@ -38,11 +38,11 @@ int main(void)
 }
 
 
-__global__ void random_kernel(int *Ad)
+__global__ void random_kernel(int *Ad, unsigned long long seed)
 {
     int i = threadIdx.x;
     int j = blockIdx.x * 1024;
-    Ad[i + j] = random_seven(i);
+    Ad[i + j] = random_seven(i, seed);
 }
 
 __global__ void sum_kernel(int *Ad)
@@ -58,29 +58,28 @@ __global__ void sum_kernel(int *Ad)
     printf("Aparaciones del %i es de: %f\n", id, result);
 }
 
-__device__ int random_seven(int thread)
-{
-    int flag = 0;
-    int result = 0;
-    int count = 0;
-    while (flag == 0)
-    {
-        int num = 5 * random_five(thread, count * 1000) + random_five(thread, count * 1000 - 1);
-        if(num < 21)
-        {
-            result = num % 7 + 1;
-            flag = 1;
-        }
-        count ++;
-    }
-    return result;
-}
-
-__device__ int random_five(int thread, int seed)
+__device__ int random_seven(int thread, unsigned long long seed)
 {
     curandState state;
-    curand_init(seed, thread, 0, &state); // 1234 is the seed, can be changed
-    int random = (curand(&state)%5) + 1;
+    curand_init(seed, thread, 0, &state);
 
-    return random;
+    int num;
+    do {
+        int a = random_five(thread, state);
+        int b = random_five(thread, state); 
+        num = a * 5 + b;     
+    } while (num >= 21);     
+    printf("%i\n", num % 7 + 1);    
+    return num % 7 + 1; 
+}
+
+__device__ int random_five(int thread, curandState state)
+{
+    int r;
+    int limit = RAND_MAX - (RAND_MAX % 5);
+    do {
+        r = (curand(&state));
+    } while (r >= limit);
+
+    return r % 5;
 }
